himanshugoyal1065/bigbluebutton

Added edge case to error handling on base classes and SFU pipeline cleanup on room empty

labs/bbb-webrtc-sfu/lib/mcs-core/lib/model/MediaSession.js
@@ -32,7 +32,7 @@ module.exports = class MediaSession {
     this._options = options;
     this._adapter = options.adapter? options.adapter : C.STRING.KURENTO;
     this._name = options.name? options.name : C.STRING.DEFAULT_NAME;
-    this._MediaServer = MediaSession.getAdapter(this._adapter);
+    this._MediaServer = MediaSession.getAdapter(this._adapter, emitter);
     this.eventQueue = [];
   }
 
@@ -147,14 +147,14 @@ module.exports = class MediaSession {
     }
   }
 
-  static getAdapter (adapter) {
+  static getAdapter (adapter, emitter) {
     let obj = null;
 
     Logger.info("[mcs-media-session] Session is using the", adapter, "adapter");
 
     switch (adapter) {
       case C.STRING.KURENTO:
-        obj = new Kurento(kurentoUrl);
+        obj = new Kurento(kurentoUrl, emitter);
         break;
       case C.STRING.FREESWITCH:
         obj = new Freeswitch();

labs/bbb-webrtc-sfu/lib/mcs-core/lib/model/Room.js
@@ -5,11 +5,14 @@
 
 'use strict'
 
+const C = require('../constants/Constants');
+
 module.exports = class Room {
-  constructor(id) {
+  constructor(id, emitter) {
     this._id = id;
     this._users = {};
     this._mcuUsers = {};
+    this.emitter = emitter;
   }
 
   getUser (id) {
@@ -21,6 +24,11 @@ module.exports = class Room {
   }
 
   destroyUser(userId) {
-    this._users[userId] = null;
+    if (this._users[userId]) {
+      delete this._users[userId];
+      if (Object.keys(this._users).length <= 0) {
+        this.emitter.emit(C.EVENT.ROOM_EMPTY, this._id);
+      }
+    }
   }
 }

bigbluebutton/bbb-webrtc-sfu

lib/mcs-core/lib/model/MediaSession.js
@@ -53,11 +53,11 @@ module.exports = class MediaSession {
     // Media server interface based on given adapter
     this._isComposedAdapter = isComposedAdapter(this._adapter);
     if (this._isComposedAdapter) {
-      this._audioAdapter = MediaSession.getAdapter(this._adapter.audio);
-      this._videoAdapter = MediaSession.getAdapter(this._adapter.video);
+      this._audioAdapter = MediaSession.getAdapter(this._adapter.audio, emitter);
+      this._videoAdapter = MediaSession.getAdapter(this._adapter.video, emitter);
     } else {
-      this._audioAdapter = MediaSession.getAdapter(this._adapter);
-      this._videoAdapter = MediaSession.getAdapter(this._adapter);
+      this._audioAdapter = MediaSession.getAdapter(this._adapter, emitter);
+      this._videoAdapter = MediaSession.getAdapter(this._adapter, emitter);
     }
     // Media server adapter ID for this session's element
     this._mediaElement;
@@ -79,7 +79,7 @@ module.exports = class MediaSession {
     this._iceSubscription = false;
   }
 
-  static getAdapter (adapter) {
+  static getAdapter (adapter, emitter) {
     let obj = null;
 
     Logger.info("[mcs-media-session] Session is using the", adapter, "adapter");
@@ -89,7 +89,7 @@ module.exports = class MediaSession {
         obj = new Freeswitch(Balancer);
         break;
       default:
-        obj = new Kurento(Balancer);
+        obj = new Kurento(Balancer, emitter);
         break;
     }
 

lib/mcs-core/lib/model/Room.js
@@ -11,7 +11,6 @@ module.exports = class Room {
   constructor(id, emitter) {
     this.id = id;
     this._users = {};
-    this._mcuUsers = {};
     this._emitter = emitter;
   }
 
@@ -30,6 +29,11 @@ module.exports = class Room {
 
   destroyUser(userId) {
     this._emitter.emit(C.EVENT.USER_LEFT, { roomId: this.id,  userId });
-    delete this._users[userId];
+    if (this._users[userId]) {
+      delete this._users[userId];
+      if (Object.keys(this._users).length <= 0) {
+        this._emitter.emit(C.EVENT.ROOM_EMPTY, this.id);
+      }
+    }
   }
 }