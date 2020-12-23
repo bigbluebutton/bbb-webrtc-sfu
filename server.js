4e96f7f2d6d97cbc93bc59dbff64b768da769ac1
prlanzarin <prlanzarin@inf.ufrgs.br>
2020-12-23 13:27:31 +0000

    process-manager -> sfu-module-manager

--- server.js
+++ server.js
@@ -3,15 +3,15 @@
 const HttpServer = require('./lib/connection-manager/HttpServer.js');
 const WebsocketConnectionManager = require('./lib/connection-manager/WebsocketConnectionManager.js');
 const ConnectionManager = require('./lib/connection-manager/ConnectionManager.js');
-const ProcessManager = require('./lib/ProcessManager.js');
+const SFUModuleManager = require('./lib/sfu-module-manager.js');
 const Logger = require('./lib/utils/Logger.js');
 
 const HTTPServer = new HttpServer();
 const WSManager = new WebsocketConnectionManager(HTTPServer.getServerObject(), '/bbb-webrtc-sfu');
 const CM = new ConnectionManager();
 
-ProcessManager.start();
-CM.setupModuleRouting(ProcessManager.modules);
+SFUModuleManager.start();
+CM.setupModuleRouting(SFUModuleManager.modules);
 CM.setHttpServer(HTTPServer);
 CM.addAdapter(WSManager);
 CM.listen(() => {
