11dcdd264e7b21393ff23d18d682d8d17031d2a1
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2022-02-08 11:48:00 -0300

    feat(ws): make websocket server options configurable, specify maxPayload (51200)
    Also includes a minor refactor to use websockets/ws built-in HTTP server
    instead of a custom one

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 'use strict';
 
 const config = require('config');
-const HttpServer = require('./lib/common/http-server.js');
 const WebsocketConnectionManager = require('./lib/main/ws-connection-manager.js');
 const ConnectionManager = require('./lib/main/connection-manager.js');
 const SFUModuleManager = require('./lib/main/sfu-module-manager.js');
-const Logger = require('./lib/utils/Logger.js');
 
 const HTTP_SERVER_HOST = config.has('clientHost') ? config.get('clientHost') : '127.0.0.1';
 const HTTP_SERVER_PORT = config.get('clientPort');
+const WS_SERVER_OPTIONS = config.has('wsServerOptions')
+  ? config.get('wsServerOptions')
+  : { maxPayload: 51200 };
 
-const HTTPServer = new HttpServer(HTTP_SERVER_HOST, HTTP_SERVER_PORT);
-HTTPServer.start();
-const WSManager = new WebsocketConnectionManager(HTTPServer.getServerObject(), '/bbb-webrtc-sfu');
+const WSManager = new WebsocketConnectionManager(
+  HTTP_SERVER_HOST,
+  HTTP_SERVER_PORT,
+  '/bbb-webrtc-sfu',
+  WS_SERVER_OPTIONS
+);
+
 const CM = new ConnectionManager();
 
 SFUModuleManager.start();
 CM.setupModuleRouting(SFUModuleManager.modules);
-CM.setHttpServer(HTTPServer);
 CM.addAdapter(WSManager);
-CM.listen(() => {
-  Logger.info("[bbb-webrtc-sfu] API transport: up");
-});
