ee2fd639604ed5117ce523e0829c1c4ab36dedb5
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2022-02-14 15:48:36 -0300

    fix: cleanup mcs-core rooms by tying them to akka-apps lifecycle
    Should avoid stuck rooms/users on the majority of scenarios

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const config = require('config');
 const WebsocketConnectionManager = require('./lib/main/ws-connection-manager.js');
 const ConnectionManager = require('./lib/main/connection-manager.js');
 const SFUModuleManager = require('./lib/main/sfu-module-manager.js');
+const Janitor = require('./lib/main/janitor.js');
 
 const HTTP_SERVER_HOST = config.has('clientHost') ? config.get('clientHost') : '127.0.0.1';
 const HTTP_SERVER_PORT = config.get('clientPort');
@@ -23,3 +24,4 @@ const CM = new ConnectionManager();
 SFUModuleManager.start();
 CM.setupModuleRouting(SFUModuleManager.modules);
 CM.addAdapter(WSManager);
+Janitor.clockIn();
